3b7dafd71e5dc97f1c2a085c0d92ebad6cc49e0a
nflanders9 <flanders.nick@gmail.com>
2014-11-13 20:47:31 -0500

    Initial Probability Values
    - Add initial values for Markov probability
    - Edit dictionary creator function to allow for comments

--- markov_chain.py
+++ markov_chain.py
@@ -1,20 +1,37 @@
 __author__ = 'Nick Flanders'
 
+import sys
+
 # Generate a Markov Probability Chain for note relationships
 
 
 # Data Definitions:
 # An MTuple is a (Number, Number)
 
-import sys
+# Initiate dictionary
+prob_dict = {}
 
-def gen_markov(line):
+
+def add_entry(line):
+    """
+
+    :rtype : Dictionary
+    """
     pair_array = line.split(" ")
-    print pair_array
+    key = pair_array[0]
+    value = float(pair_array[1])
+    key_array = key.strip("(").strip(")").split(",")
+    prob_dict[(key_array[0], key_array[1])] = value
+
 
 prob_file = open("markov_init.txt", 'r').read()
 split_lines = prob_file.splitlines()
 for line in split_lines:
-    gen_markov(line)
+    if line[0] != "#":
+        add_entry(line)
+
+print prob_dict
+
+print prob_dict[('I','vii0')]
 
 
