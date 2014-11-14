3a44bf35e0465cb4329811bcd2a866109a922fe1
nflanders9 <flanders.nick@gmail.com>
2014-11-13 21:59:01 -0500

    Add Markov Data Checker
    - Add valid_input to markov_chain.py to test if data in markov_init.txt
    is valid
    - Reformat markov_chain.py to allow importing the Markov chain into
    other files by calling markov()

--- markov_chain.py
+++ markov_chain.py
@@ -1,20 +1,15 @@
-__author__ = 'Nick Flanders'
-
+import math
 import sys
+
+__author__ = 'Nick Flanders'
 
 # Generate a Markov Probability Chain for note relationships
 
 
-# Data Definitions:
-# An MTuple is a (Number, Number)
-
-# Initiate dictionary
-prob_dict = {}
-
-
 def add_entry(line):
     """
-
+    Parse the string containing the dictionary data and add the value to the dictionary
+    :param line: String
     :rtype : Dictionary
     """
     pair_array = line.split(" ")
@@ -24,14 +19,47 @@ def add_entry(line):
     prob_dict[(key_array[0], key_array[1])] = value
 
 
-prob_file = open("markov_init.txt", 'r').read()
-split_lines = prob_file.splitlines()
-for line in split_lines:
-    if line[0] != "#":
-        add_entry(line)
 
-print prob_dict
+def valid_input(list_of_lines):
+    """
+    Determine if the Markov chain data is valid
+    :rtype : Boolean
+    :param list_of_lines:
+    """
+    valid = True
+    list_of_probs = []
+    for line in list_of_lines:
+        if line[0] == "@":
+            if math.fabs(sum(list_of_probs) - 1.0) > .001:
+                valid = False
+                break
+            else:
+                list_of_probs = []
+        if line[0] == "(":
+            prob = float(line.split(" ")[1])
+            list_of_probs.append(prob)
+    return valid
 
-print prob_dict[('I','vii0')]
 
 
+
+
+def markov():
+    """
+    Return a dictionary with the probability data in markov_init.txt
+    :rtype: Dictionary
+    """
+    # Initiate dictionary
+    global prob_dict
+    prob_dict = {}
+    prob_file = open("markov_init.txt", 'r').read()
+    split_lines = prob_file.splitlines()
+    if valid_input(split_lines):
+        for line in split_lines:
+            if line[0] != "#" and line[0] != "@":
+                add_entry(line)
+        return prob_dict
+    else:
+        return 'Error: Invalid probability values in Markov Chain data file.'
+
+print markov()
